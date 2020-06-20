d458d49daf1fe7d5284f96842c0de5042729094b
Dan Stanescu <stanescu@uwyo.edu>
2020-06-19 18:29:52 -0600

    Rename plus comments.

--- src/lady_or_tiger.lean
+++ src/lady_or_tiger.lean
@@ -1,5 +1,15 @@
 import tactic
 
+/-
+The first three puzzles from:
+    "The Lady or the Tiger? And Other Logic Puzzles"
+        by Raymond Smullyan.
+Contributed to the Lean Zulip chat by Yury G. Kudryashov
+but apparently set up by his seven-years-old son.
+Slightly modified in appearance but not in content.
+
+-/
+
 inductive door_leads_to
 | lady
 | tiger
@@ -14,14 +24,6 @@ def Q1 := Q
 namespace Q1
 
 variables (q : Q1)
-#check q
-#check Q1
-#check Q
-#check Q.d₁
-#check q.d₁
-variable p : Q
-#check p.1
-lemma qq : Q = Q1 := rfl
 
 def D1 := q.1 = y ∧ q.2 = n
 
@@ -31,29 +33,8 @@ def H := q.D1 ∧ ¬q.D2 ∨ ¬q.D1 ∧ q.D2
 
 lemma answer_term : q.H → q.1 = n ∧ q.2 = y :=
 by rcases q with ⟨_|_,_|_⟩; simp [H, D1, D2]
-#print answer_term
 
-lemma answer_tactic1 : q.H → q.1 = n ∧ q.2 = y :=
-begin
-    intro h,
-    split,
-    cases h with h1 h2,
-    cases h1 with h11 h12,
-    cases h11 with h111 h112,
-    simp [D2] at h12,
-    have g1 : q.d₁ = y ∨ q.d₂ = y, from or.inl h111,
-    --have g2 : h12 g1,
-    cases g1 with ha hb, sorry, sorry, sorry, sorry, 
-end
-
-lemma answer_tactic2 : q.H → q.1 = n ∧ q.2 = y :=
-begin
-    rcases q with ⟨_|_,_|_⟩;
-    simp [H, D1, D2],
-    done
-end
-
-lemma answer_tactic3: q.H → q.1 = n ∧ q.2 = y :=
+lemma answer_tactic : q.H → q.1 = n ∧ q.2 = y :=
 begin
     rcases q with ⟨_|_,_|_⟩,
     simp [H], simp [D1], simp [D2],
