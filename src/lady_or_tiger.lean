53c046af46550618b1a67b55be6a90fa5feda8a8
Dan Stanescu <stanescu@uwyo.edu>
2020-06-21 13:23:04 -0600

    Puzzle 6, comments

--- src/lady_or_tiger.lean
+++ src/lady_or_tiger.lean
@@ -82,12 +82,20 @@ by rcases q with ⟨_|_,_|_⟩; simp [H, D1, D2]
 end Q3
 
 /-
-The next five lady/tiger puzzles from the same book:
+Puzzles 4-7 from the same book:
     "The Lady or the Tiger? And Other Logic Puzzles"
         by Raymond Smullyan.
 Solutions written by D. Stanescu in the same framework as above. 
+In these puzzles, the king explains that the sign on the first door (D1) is true if a lady
+is in in that room and is false if a tiger is in that room. The opposite is true for the sign on 
+the second door (D2), which is true if a tiger is hidden behind it but false otherwise.
 -/
 
+/-
+Puzzle number four: 
+First door sign reads: "Both rooms contain ladies." 
+Second door sign reads: "Both rooms contain ladies."
+-/
 
 def Q4 := Q
 /- ∧ : \and ∨ : \or ¬ : \not -/
@@ -100,7 +108,7 @@ def D1 := q.1=y ∧ q.2=y
 
 def D2 := q.1=y ∧ q.2=y
 
--- one way to set up this problem
+-- one way to set up this problem 
 def H1 := q.1 = y ∧ q.D1 ∨ q.1 = n ∧ ¬ q.D1
 def H2 := q.2 = y ∧ ¬ q.D2 ∨ q.2 = n ∧ q.D2
 def H := q.H1 ∧ q.H2
@@ -122,6 +130,11 @@ begin
     done
 end
 
+/-
+Puzzle number five: 
+First door sign reads: "At least one room contains a lady." 
+Second door sign reads: "The other room contains a lady."
+-/
 
 end Q4
 
@@ -136,7 +149,7 @@ def D1 := q.1=y ∨ q.2=y ∨ q.1 = y ∧ q.2 = y
 
 def D2 := q.1=y
 
--- same set up as above
+-- same setup as Q4 above
 def H1 := q.1 = y ∧ q.D1 ∨ q.1 = n ∧ ¬ q.D1
 def H2 := q.2 = y ∧ ¬ q.D2 ∨ q.2 = n ∧ q.D2
 def H := q.H1 ∧ q.H2
@@ -148,5 +161,37 @@ begin
     done
 end
 
-
 end Q5
+
+/-
+Puzzle number six: 
+First door sign reads: "It makes no difference which room you pick." 
+Second door sign reads: "There is a lady in the other room."
+Apparently he king is particularly fond of this puzzle.
+-/
+
+def Q6 := Q
+/- ∧ : \and ∨ : \or ¬ : \not -/
+
+namespace Q6
+
+variables (q : Q6)
+
+def D1 := q.1 = q.2
+
+def D2 := q.1=y
+
+-- same setup as Q4 above
+def H1 := q.1 = y ∧ q.D1 ∨ q.1 = n ∧ ¬ q.D1
+def H2 := q.2 = y ∧ ¬ q.D2 ∨ q.2 = n ∧ q.D2
+def H := q.H1 ∧ q.H2
+
+lemma answer : q.H → q.1 = n ∧ q.2 = y :=
+begin
+    rcases q with ⟨_|_,_|_⟩;
+    simp [H, H1, D1, H2, D2], 
+    done
+end
+
+
+end Q6
